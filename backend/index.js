fcafcd1c2124e48091d97d86037ea1eaa2711829
Saurabh Kumar <saurabhrt18@gmail.com>
2026-01-14 20:16:26 +0000

    Fix backend dependencies and deployment setup

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'http://localhost:3000',
+    origin:'https://chat-app23.onrender.com',
     credentials:true
 };
 app.use(cors(corsOption)); 
